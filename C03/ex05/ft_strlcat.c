56ec8719a2ba454b715c8a7524896284f68859ce
Mehdi El Ouariachi <meel-oua@c2r1p2.42lehavre.fr>
2023-11-18 16:12:50 +0100

    ok

--- C03/ex05/ft_strlcat.c
+++ C03/ex05/ft_strlcat.c
@@ -6,46 +6,47 @@
 /*   By: meel-oua <meel-oua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/18 14:37:59 by meel-oua          #+#    #+#             */
-/*   Updated: 2023/11/18 14:54:34 by meel-oua         ###   ########.fr       */
+/*   Updated: 2023/11/18 15:00:52 by meel-oua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	if (size == 0)
+		return (NULL);
+	while (dest[i])
+		i++;
+	while (j < size - 1 && src[j])
+	{
+		dest[i] == src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (ft_strlen(dest) + ft_strlen(src));
+}
+
 #include <stdio.h>
 
-int ft_strlen(char *str) {
-    int i = 0;
-    while (str[i]) {
-        i++;
-    }
-    return i;
-}
-
-unsigned int ft_strlcat(char *dest, char *src, unsigned int n) {
-    unsigned int i = 0;
-    unsigned int j = 0;
-
-    while (dest[i]) {
-        i++;
-    }
-
-    while (j < n - 1 && src[j]) {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-
-    dest[i] = '\0';
-
-    return i;
-}
-
-int main() {
-    char dest[] = "Empereur";
-    char src[] = "Baggy";
-    
-    printf("%u\n", ft_strlcat(dest, src, 5));
-    printf("Resultat : %s\n", dest);
-
-    return 0;
+int main()
+{
+	char dest[] = "Empereur";
+	char src[] = "Baggy";
+	printf("%d", ft_strlcat(dest, src, 5));
 }
